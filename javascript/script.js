53c8777b516299dfbf9adc334aa5e70fb2ae34bc
NavjitG <32511148+NavjitG@users.noreply.github.com>
2017-12-29 17:38:59 +0000

    maps api and feedback form added to contact page

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,23 @@
 
+function initMap() {
+  let leicester = {lat: 52.6333, lng: -1.1333};
+  let trueGamer = {lat: 52.629311, lng: -1.137836};
+  let mapDemo = document.getElementById("map");
+
+  // When the map is loaded this will set the default location 
+  let map = new google.maps.Map(mapDemo, {
+    zoom: 13,
+    center: leicester
+  });
+
+  // This sets a marker on the mnap showing the loacation of the office
+  let markerDMU = new google.maps.Marker({
+    position: trueGamer,
+    map: map,
+    title: 'TrueGamer Office'
+  });
+}
+
 
 let sliderImages = document.querySelectorAll('.slide'),
     arrowLeft = document.querySelector('#arrow-left'),
